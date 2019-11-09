a941a78be1509a9ffb1340f3c7204305c7142f66
BUGLAN <1831353087@qq.com>
2019-11-09 15:43:46 +0800

    update: 删除巨量文档 | 添加pipfile

---
+++ README.md
@@ -0,0 +1,15 @@
+## flask 源码解析
+
+> 兴趣使然的源码解析
+
+## Flask
+
+### Flask.run()
+
+> flask wsgi server 启动器
+
+
+## Response
+
+
+## Request
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 # 初始化Flask这个类
-# __name__ 是import_name, Flask 会更具这个name获取到当前路径
+# __name__ 是import_name, Flask 会根据这个name获取到当前路径
 
 
 @app.route("/")
