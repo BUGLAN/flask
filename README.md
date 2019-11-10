b40c8c5e2ec39a2bba204114c496c9fd083bb0e5
BUGLAN <1831353087@qq.com>
2019-11-10 14:57:59 +0800

    update: 删除巨量文档 | 添加pipfile

--- README.md
+++ README.md
@@ -2,11 +2,61 @@
 
 > 兴趣使然的源码解析
 
-## Flask
+## _PackageBoundObject
+
+用于绑定类属性, 在`Flask`和`Blueprint`这两个类都有使用到
+
+重要代码
+```python
+_PackageBoundObject.__init__(
+            self,
+            import_name,
+            template_folder=template_folder,
+            root_path=root_path
+        )
+```
+
+## Flask 类
 
 ### Flask.run()
 
+
+关键代码
+
+```python
+def wsgi_app(self):
+    ctx = self.request_context(environ)
+    error = None
+    try:
+        try:
+            ctx.push()
+            response = self.full_dispatch_request()
+        except Exception as e:
+            error = e
+            response = self.handle_exception(e)
+        except:
+            error = sys.exc_info()[1]
+            raise
+        return response(environ, start_response)
+    finally:
+        if self.should_ignore_error(error):
+            error = None
+        ctx.auto_pop(error)
+```
+
+一行一行解释:
+
+`ctx = self.request_context(environ)`
+
+
 > flask wsgi server 启动器
+
+
+## wsgi
+
+wsgi server gateway
+
+wsgi application / framework
 
 
 ## Response
--- flask/app.py
+++ flask/app.py
@@ -596,63 +596,7 @@ class Flask(_PackageBoundObject):
 
     def run(self, host=None, port=None, debug=None,
             load_dotenv=True, **options):
-        """Runs the application on a local development server.
 
-        Do not use ``run()`` in a production setting. It is not intended to
-        meet security and performance requirements for a production server.
-        Instead, see :ref:`deployment` for WSGI server recommendations.
-
-        If the :attr:`debug` flag is set the server will automatically reload
-        for code changes and show a debugger in case an exception happened.
-
-        If you want to run the application in debug mode, but disable the
-        code execution on the interactive debugger, you can pass
-        ``use_evalex=False`` as parameter.  This will keep the debugger's
-        traceback screen active, but disable code execution.
-
-        It is not recommended to use this function for development with
-        automatic reloading as this is badly supported.  Instead you should
-        be using the :command:`flask` command line script's ``run`` support.
-
-        .. admonition:: Keep in Mind
-
-           Flask will suppress any server error with a generic error page
-           unless it is in debug mode.  As such to enable just the
-           interactive debugger without the code reloading, you have to
-           invoke :meth:`run` with ``debug=True`` and ``use_reloader=False``.
-           Setting ``use_debugger`` to ``True`` without being in debug mode
-           won't catch any exceptions because there won't be any to
-           catch.
-
-        :param host: the hostname to listen on. Set this to ``'0.0.0.0'`` to
-            have the server available externally as well. Defaults to
-            ``'127.0.0.1'`` or the host in the ``SERVER_NAME`` config variable
-            if present.
-        :param port: the port of the webserver. Defaults to ``5000`` or the
-            port defined in the ``SERVER_NAME`` config variable if present.
-        :param debug: if given, enable or disable debug mode. See
-            :attr:`debug`.
-        :param load_dotenv: Load the nearest :file:`.env` and :file:`.flaskenv`
-            files to set environment variables. Will also change the working
-            directory to the directory containing the first file found.
-        :param options: the options to be forwarded to the underlying Werkzeug
-            server. See :func:`werkzeug.serving.run_simple` for more
-            information.
-
-        .. versionchanged:: 1.0
-            If installed, python-dotenv will be used to load environment
-            variables from :file:`.env` and :file:`.flaskenv` files.
-
-            If set, the :envvar:`FLASK_ENV` and :envvar:`FLASK_DEBUG`
-            environment variables will override :attr:`env` and
-            :attr:`debug`.
-
-            Threaded mode is enabled by default.
-
-        .. versionchanged:: 0.10
-            The default port is now picked from the ``SERVER_NAME``
-            variable.
-        """
         # Change this into a no-op if the server is invoked from the
         # command line. Have a look at cli.py for more information.
         if os.environ.get('FLASK_RUN_FROM_CLI') == 'true':
